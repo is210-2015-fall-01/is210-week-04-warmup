fb16a4db6c40a21abdd64e83ca8f6e4707781bce
Christopher Fernanders <cj.fernanders1986@gmail.com>
2015-09-17 03:25:40 +0000

    fixed task 4

--- task_04.py
+++ task_04.py
@@ -2,21 +2,28 @@
 # -*- coding: utf-8 -*-
 """defining a function with three parameters"""
 
+
 def too_many_kittens(kittens, litterboxes, catfood):
-    """Declares three parameters.
+    """Declares three parameters for cats.
 
     Args:
-       kittens(mixed): First parameter denoting number of kittens
-       litterboxes(int): a parameter defining number of available litterboxes.
-       catfood(bool): denoting if there is any catfood.
-     Returns:
-      a boolean, True if successful, False if otherwise.
-    Examples:
+         kittens(mixed): First parameter denoting number of kittens
+         litterboxes(int): a parameter defining number of available litterboxes.
+         catfood(bool): denoting if there is any catfood.
+
+    Returns:
+         a boolean, True if successful, False if otherwise.
+
+
+     Examples:
+
        >>> too_many_kittens(12, 12, False)
         True
-       >>> too_many_kittens(13, 12, True)
+
+    >>> too_many_kittens(13, 12, True)
         True
-        >>> too_many_kittens(12, 13, True)
+
+    >>> too_many_kittens(12, 13, True)
         False
-   """
+    """
     return not(litterboxes >= kittens and catfood)
